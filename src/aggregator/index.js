9db108edbb3e10608df8d8ca168eb1092b30cc2d
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-01-06 22:07:11 +0100

    Creates dummy aggregator.

---
+++ src/aggregator/index.js
@@ -0,0 +1,13 @@
+const { safeDump: toYaml, safeLoad: fromYaml } = require('js-yaml')
+const getStdin = require('get-stdin')
+const { run } = require('../utils')
+
+const deduplicate = events => events
+
+const aggregate = yaml =>
+    toYaml (deduplicate (fromYaml (yaml)))
+
+if (require.main === module) run (() => getStdin ().then (aggregate))
+
+module.exports =
+    { aggregate }
