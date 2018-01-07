43ab352ba978c44169a7e12efbc569d0dee5319b
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-01-07 01:07:36 +0100

    Enhances dummy aggregator to remove invalid entries and sort them by date.

--- src/aggregator/index.js
+++ src/aggregator/index.js
@@ -1,12 +1,21 @@
 const { safeDump: toYaml, safeLoad: fromYaml } = require('js-yaml')
 const getStdin = require('get-stdin')
-const { run } = require('../utils')
+const { run, filter } = require('../utils')
 
 const deduplicate = events =>
     events
 
+const sort = events =>
+    events.sort (({startDate: dateA}, {startDate: dateB}) => Date.parse(dateA) - Date.parse(dateB))
+
+const validIfSet = date =>
+    date ? date.length === 10 : true
+
+const saneEvent = ({name, startDate, endDate, cfpEndDate}) =>
+    name && startDate && validIfSet (startDate) && validIfSet (endDate) && validIfSet (cfpEndDate)
+
 const aggregate = yaml =>
-    toYaml (deduplicate (fromYaml (yaml)))
+    toYaml (sort (deduplicate (filter (saneEvent) (fromYaml (yaml)))))
 
 if (require.main === module) run (() => getStdin ().then (aggregate))
 
